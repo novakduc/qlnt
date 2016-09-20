c3e0b3eb04bd1a79f068af92fee4c05273d963fe
novakduc <novakduc@gmail.com>
2016-09-20 23:46:29 +0700

    389/3/4a

--- app/src/main/java/com/novakduc/forbega/qlnt/model/Qlnt.java
+++ app/src/main/java/com/novakduc/forbega/qlnt/model/Qlnt.java
@@ -1,12 +1,16 @@
 package com.novakduc.forbega.qlnt.model;
 
+import android.content.Context;
+
 /**
  * Created by n.thanh on 9/20/2016.
  */
 public class Qlnt {
     private static Qlnt ourInstance = new Qlnt();
+    private static Context sContext;
 
-    public static Qlnt getInstance() {
+    public static Qlnt getInstance(Context context) {
+        sContext = context;
         return ourInstance;
     }
 
