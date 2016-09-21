d2577e5d13a25993211fabc189aa78bd23c008a9
novakduc <novakduc@gmail.com>
2016-09-21 10:11:14 +0700

    startup

---
+++ app/src/main/java/com/novakduc/forbega/qlnt/model/Project.java
@@ -0,0 +1,17 @@
+package com.novakduc.forbega.qlnt.model;
+
+import java.util.Date;
+
+/**
+ * Created by n.thanh on 9/21/2016.
+ */
+
+public class Project {
+    private String mName;
+    private String mAddress;
+    private Long mInvestment;
+    private Long mTotalIncome;
+    private Date mStartDate;
+    private Date mEndDate;
+    private UnitPrice mUnitPrice;
+}
