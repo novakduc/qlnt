437af00585ef9348df4004679dd57f18548bcd7c
novakduc <novakduc@gmail.com>
2016-09-28 23:46:16 +0700

    mobile lap

---
+++ app/src/test/java/com/novakduc/forbega/qlnt/TabFragmentTest.java
@@ -0,0 +1,31 @@
+package com.novakduc.forbega.qlnt;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by n.thanh on 9/28/2016.
+ */
+public class TabFragmentTest {
+    @Test
+    public void getInstance() throws Exception {
+
+    }
+
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+    @Test
+    public void onCreateView() throws Exception {
+
+    }
+
+    @Test
+    public void onViewCreated() throws Exception {
+
+    }
+
+}
