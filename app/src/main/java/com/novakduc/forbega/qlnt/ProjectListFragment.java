4214f4f8fb5443b956e48cf0e0409a16e65abf6f
novakduc <novakduc@gmail.com>
2016-09-29 23:52:54 +0700

    mobile lap

---
+++ app/src/main/java/com/novakduc/forbega/qlnt/ProjectListFragment.java
@@ -0,0 +1,11 @@
+package com.novakduc.forbega.qlnt;
+
+import android.app.ListFragment;
+
+/**
+ * Created by n.thanh on 9/29/2016.
+ */
+
+public class ProjectListFragment extends ListFragment {
+    // TODO: 9/29/2016
+}
