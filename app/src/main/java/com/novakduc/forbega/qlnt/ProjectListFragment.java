3c8b016465834ff78679175bb51eddf84abe3322
novakduc <novakduc@gmail.com>
2016-10-20 16:25:21 +0700

    tdt

--- app/src/main/java/com/novakduc/forbega/qlnt/ProjectListFragment.java
+++ app/src/main/java/com/novakduc/forbega/qlnt/ProjectListFragment.java
@@ -2,11 +2,11 @@ package com.novakduc.forbega.qlnt;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -89,8 +89,9 @@ public class ProjectListFragment extends Fragment {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "FAB clicked!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(getActivity(), ProjectConfigurationActivity.class);
+                startActivity(intent);
+                // TODO: 10/20/2016 Register activity
             }
         });
 
