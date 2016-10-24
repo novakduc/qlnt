4c4599100bfd481800ca0e19c375f291be8ddb27
novakduc <novakduc@gmail.com>
2016-10-24 16:57:49 +0700

    tdt

--- app/src/main/java/com/novakduc/forbega/qlnt/ProjectBaseConfigFragment.java
+++ app/src/main/java/com/novakduc/forbega/qlnt/ProjectBaseConfigFragment.java
@@ -3,6 +3,8 @@ package com.novakduc.forbega.qlnt;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,6 +37,13 @@ public class ProjectBaseConfigFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_project_base_config, container, false);
+
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+        activity.getSupportActionBar().setHomeAsUpIndicator(R.drawable.cheese_1);
+        activity.getSupportActionBar().setHomeButtonEnabled(true);
+
         return view;
     }
 }
