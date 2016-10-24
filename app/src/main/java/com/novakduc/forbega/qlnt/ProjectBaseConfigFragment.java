140f4dd19a2482b2760bd8892e5b70bb1f75c2ad
novakduc <novakduc@gmail.com>
2016-10-24 15:06:03 +0700

    tdt

--- app/src/main/java/com/novakduc/forbega/qlnt/ProjectBaseConfigFragment.java
+++ app/src/main/java/com/novakduc/forbega/qlnt/ProjectBaseConfigFragment.java
@@ -1,13 +1,11 @@
 package com.novakduc.forbega.qlnt;
 
 import android.app.Fragment;
-import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.novakduc.forbega.qlnt.model.Project;
 
@@ -19,6 +17,14 @@ public class ProjectBaseConfigFragment extends Fragment {
     public static final String TEMP_PROJECT = "com.novakduc.forbega.qlnt.tempproject";
     private Project mProject;
 
+    public static ProjectBaseConfigFragment newInstance(Project tempProject) {
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(TEMP_PROJECT, tempProject);
+        ProjectBaseConfigFragment fragment = new ProjectBaseConfigFragment();
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,23 +35,6 @@ public class ProjectBaseConfigFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_project_base_config, container, false);
-        Button button = (Button) view.findViewById(R.id.next);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager manager = getActivity().getFragmentManager();
-                manager.beginTransaction().replace(R.id.fragmentContainer,
-                        ProjectFinanceConfigFragment.newInstance(mProject)).commit();
-            }
-        });
         return view;
-    }
-
-    public static ProjectBaseConfigFragment newInstance(Project tempProject) {
-        Bundle bundle = new Bundle();
-        bundle.putSerializable(TEMP_PROJECT, tempProject);
-        ProjectBaseConfigFragment fragment = new ProjectBaseConfigFragment();
-        fragment.setArguments(bundle);
-        return fragment;
     }
 }
