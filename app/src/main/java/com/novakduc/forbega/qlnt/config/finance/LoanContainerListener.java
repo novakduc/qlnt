563a29843fa141894012d92d87d15e8672795973
novakduc <novakduc@gmail.com>
2018-01-11 14:02:41 +0700

    LL

--- app/src/main/java/com/novakduc/forbega/qlnt/config/finance/LoanContainerListener.java
+++
@@ -1,9 +0,0 @@
-package com.novakduc.forbega.qlnt.config.finance;
-
-/**
- * Created by Nguyen Quoc Thanh on 12/26/2017.
- */
-
-public interface LoanContainerListener {
-    void loanDeleteUpdate();
-}
--- app/src/main/java/com/novakduc/forbega/qlnt/config/finance/LoanDeclareFragmentListener.java
+++
@@ -1,9 +0,0 @@
-package com.novakduc.forbega.qlnt.config.finance;
-
-/**
- * Created by Novak on 4/30/2017.
- */
-
-public interface LoanDeclareFragmentListener {
-    void discardConfirm();
-}
