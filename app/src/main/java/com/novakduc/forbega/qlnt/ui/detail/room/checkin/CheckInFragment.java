1bae0938938b10a03eefda6a0117048a373a8d3a
novakduc <novakduc@gmail.com>
2018-09-05 11:43:48 +0700

    ll

---
+++ app/src/main/java/com/novakduc/forbega/qlnt/ui/detail/room/checkin/CheckInFragment.java
@@ -0,0 +1,29 @@
+package com.novakduc.forbega.qlnt.ui.detail.room.checkin;
+
+import android.support.v4.app.Fragment;
+
+import com.novakduc.forbega.qlnt.utilities.ItemListAdapterActionHandler;
+
+public class CheckInFragment extends Fragment implements ItemListAdapterActionHandler {
+
+
+    @Override
+    public void onDeleteAction(long id) {
+
+    }
+
+    @Override
+    public void onCopyAction(long id) {
+
+    }
+
+    @Override
+    public void onEditAction(long id) {
+
+    }
+
+    @Override
+    public void onItemClick(long id) {
+
+    }
+}
