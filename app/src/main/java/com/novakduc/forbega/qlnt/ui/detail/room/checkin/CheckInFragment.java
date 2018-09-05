4d67c269c36fd3475904f7d209ace248d0dc914b
novakduc <novakduc@gmail.com>
2018-09-05 12:00:11 +0700

    ll

--- app/src/main/java/com/novakduc/forbega/qlnt/ui/detail/room/checkin/CheckInFragment.java
+++ app/src/main/java/com/novakduc/forbega/qlnt/ui/detail/room/checkin/CheckInFragment.java
@@ -1,11 +1,29 @@
 package com.novakduc.forbega.qlnt.ui.detail.room.checkin;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.novakduc.forbega.qlnt.utilities.ItemListAdapterActionHandler;
 
 public class CheckInFragment extends Fragment implements ItemListAdapterActionHandler {
+    public static final String ACTIVE_PROJECT_ID = "active_project_id";
+    private static final String LOG_TAG = CheckInFragment.class.getSimpleName();
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 
     @Override
     public void onDeleteAction(long id) {
