bf60edaf097761037ae311b9c7b7907eb46899a8
novakduc <novakduc@gmail.com>
2018-12-26 11:32:06 +0700

    LL

--- app/src/main/java/com/novakduc/forbega/qlnt/ui/list/NewProjectListFragment.kt
+++ app/src/main/java/com/novakduc/forbega/qlnt/ui/list/NewProjectListFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.novakduc.forbega.qlnt.databinding.FragmentProjectListBinding
+import com.novakduc.forbega.qlnt.databinding.NewFragmentProjectListBinding
 import com.novakduc.forbega.qlnt.utilities.InjectorUtils
 
 class NewProjectListFragment: Fragment() {
@@ -16,14 +17,14 @@ class NewProjectListFragment: Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentProjectListBinding.inflate(inflater, container, false)
+        val binding = NewFragmentProjectListBinding.inflate(inflater, container, false)
         val adapter = NewProjectListAdapter(binding.root.context)
         binding.rvProjectList.adapter = adapter
         subscribeUi(adapter, binding)
         return binding.root
     }
 
-    private fun subscribeUi(adapter: NewProjectListAdapter, binding: FragmentProjectListBinding?) {
+    private fun subscribeUi(adapter: NewProjectListAdapter, binding: NewFragmentProjectListBinding?) {
         val factory = InjectorUtils.provideProjectListViewModelFactory(requireContext())
         val viewModel = ViewModelProviders.of(this, factory)
                 .get(ProjectListFragmentViewModel::class.java)
